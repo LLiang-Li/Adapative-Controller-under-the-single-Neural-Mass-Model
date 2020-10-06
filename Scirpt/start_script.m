547bd975ceacfa56f50c089d0128552fa565d6f6
Lige <59388472+LLiang-Li@users.noreply.github.com>
2020-10-06 21:27:54 +0800

    Update start_script.m

--- Scirpt/start_script.m
+++ Scirpt/start_script.m
@@ -5,3 +5,4 @@ disp('The process of refresh may cost much time.')
 disp('if you don''t hope this, you could comment sl_refresh_customizations in start_script.m')
 sl_refresh_customizations
 disp('Refresh finish')
+open_system('single_NMM_Adaptive_Controller')
